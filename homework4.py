1552674d67fe2ae91c8c607c9af49e4a8cc8f297
S1NDIK4T <Sindikatas01@gmail.com>
2024-05-28 14:29:13 +0400

    homework4

--- homework4.py
+++ homework4.py
@@ -1,4 +1,5 @@
+# string methods
 my_string = input('Вводите что хотите >>> ')
 print(f"{my_string.upper()}\n{my_string.lower()}\n{my_string.replace(' ','')}\n{my_string[0]}\n{my_string[-1]}")
-
+#
 
