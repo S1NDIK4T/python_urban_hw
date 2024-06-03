926752c9ef7645265b039f6a5c1771de9e68dff9
S1NDIK4T <Sindikatas01@gmail.com>
2024-06-03 15:19:41 +0400

    правки

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
-
-print('Задача 1')
-a = input("Введите любую строку\n")
-print("Длина строки - " + str(len(a)))
-
-print('Задача 2')
-first = int(input("Введите число >>>"))
-second = int(input("Введите число >>>"))
-summa = first+second
-diff = first-second
-print("Сумма = " + str(summa) + "\nРазность = " + str(diff))
-
-print('Задача 3')
-first = int(input("Введите число >>>"))
-second = int(input("Введите число >>>"))
-third = int(input("Введите число >>>"))
-mean = (first+second+third)/3
-print(f"Среднее арифм. = {mean}")
-
-print('Задача 4')
-first_string = "Вторник"
-second_string = "Понедельник"
-print(f"{second_string}, {first_string}")
-
-print('Задача 5')
-a = int(input("Введите число >>>"))
-b = int(input("Введите число >>>"))
-c = int(input("Введите число >>>"))
-f = (a*b)+(a*c)
-print(f**3/2)
-
-
-
-surname = input('Введите вашу фамилию - ')
-name = input('Введите ваше имя - ')
-patronymic = input('Введите ваше отчество - ')
-print(f"{surname} {name[0]}.{patronymic[0]}.")
+print(5%3)
+# print('Задача 1')
+# a = input("Введите любую строку\n")
+# print("Длина строки - " + str(len(a)))
+#
+# print('Задача 2')
+# first = int(input("Введите число >>>"))
+# second = int(input("Введите число >>>"))
+# summa = first+second
+# diff = first-second
+# print("Сумма = " + str(summa) + "\nРазность = " + str(diff))
+#
+# print('Задача 3')
+# first = int(input("Введите число >>>"))
+# second = int(input("Введите число >>>"))
+# third = int(input("Введите число >>>"))
+# mean = (first+second+third)/3
+# print(f"Среднее арифм. = {mean}")
+#
+# print('Задача 4')
+# first_string = "Вторник"
+# second_string = "Понедельник"
+# print(f"{second_string}, {first_string}")
+#
+# print('Задача 5')
+# a = int(input("Введите число >>>"))
+# b = int(input("Введите число >>>"))
+# c = int(input("Введите число >>>"))
+# f = (a*b)+(a*c)
+# print(f**3/2)
+#
+#
+#
+# surname = input('Введите вашу фамилию - ')
+# name = input('Введите ваше имя - ')
+# patronymic = input('Введите ваше отчество - ')
+# print(f"{surname} {name[0]}.{patronymic[0]}.")
